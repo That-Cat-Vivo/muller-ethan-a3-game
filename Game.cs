a85ced35a88b6a48b29ca5b0e29fa524ca7b6e6d
Ethan Muller <eman3001@yahoo.com>
2024-10-29 20:03:53 -0400

    Set title and window size. Added Variables for player position and gravity. Made two objects for early collision testing.

--- Game.cs
+++ Game.cs
@@ -11,14 +11,17 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        float Gravity = 0;
+        float playPositionX = 400;
+        float playPositionY = 400;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(800, 800);
+            Window.SetTitle("Monkey Jump");
         }
 
         /// <summary>
@@ -26,6 +29,15 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
+
+            //Player Object
+            Draw.FillColor = Color.LightGray;
+            Draw.Rectangle(playPositionX, playPositionY, 40, 80);
+
+            //Temp Collision Platform
+            Draw.FillColor = Color.DarkGray;
+            Draw.Rectangle(20, 750, 760, 20);
 
         }
     }
